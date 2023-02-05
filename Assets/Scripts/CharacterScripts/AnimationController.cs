52245e7a56fd75fec2cc29c2abbcd452777beb53
DYLANPC\kav02 <dkavanagh@dtmglobal.com>
2023-02-05 10:49:36 +0000

    Scripts for controlling animations, and working on knockback of the player.

---
+++ Assets/Scripts/CharacterScripts/AnimationController.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    private bool _isJumping, _isWalking, _isRunning;
+    public Animator Controller;
+    private static readonly int Jumping = Animator.StringToHash("_isJumping");
+    private static readonly int Walking = Animator.StringToHash("_isWalking");
+    private static readonly int Running = Animator.StringToHash("_isRunning");
+    
+
+    public bool IsJumping
+    {
+        get
+        {
+            return _isJumping;
+        }
+        set
+        {
+            Controller.SetBool(Jumping, value);
+            _isJumping = value;
+        }
+    }
+
+    public bool IsWalking
+    {
+        get
+        {
+            return _isWalking;
+        }
+        set
+        {
+            Controller.SetBool(Walking, value);
+            _isWalking = value;
+        }
+    }
+    public bool IsRunning
+    {
+        get
+        {
+            return _isRunning;
+        }
+        set
+        {
+            Controller.SetBool(Running, value);
+            _isRunning = value;
+        }
+    }
+
+    public int GetStateAsInt()
+    {
+        int state = 0;
+        if (IsJumping) state += 1;
+        if (IsWalking) state += 2;
+        if (IsRunning) state += 4;
+        return state;
+    }
+
+    public void SetState(int value)
+    {
+        if (value > 4)
+        {
+            value -= 4;
+            IsRunning = true;
+        }
+        if (value > 2)
+        {
+            value -= 2;
+            _isWalking = true;
+        }
+        if (value > 1)
+        {
+            value -= 1;
+            IsJumping = true;
+        }
+    }
+    
+
+
+    
+}
